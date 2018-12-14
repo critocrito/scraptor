3970720b448b1ca7b0d2b28b8ffcd425ac1bbefa
Christo <christo@cryptodrunks.net>
2018-12-15 00:46:13 +0100

    support input fields filling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // export * from "./browser";
 import {ofP} from "dashp";
-import {curry2} from "@critocrito/curry";
+import {curry2, curry3} from "@critocrito/curry";
 import puppeteer from "puppeteer";
 
 const browse = curry2("browse", async (url, page) => {
@@ -11,12 +11,26 @@ const screenshot = curry2("screenshot", async (target, page) => {
   await page.screenshot({path: target, fullPage: true});
 });
 
+const input = curry3("input", async (selector, value, page) => {
+  /* istanbul ignore next */
+  await page.$eval(
+    selector,
+    (el, val) => {
+      el.value = ""; // eslint-disable-line no-param-reassign
+      el.value = val; // eslint-disable-line no-param-reassign
+      el.setAttribute("value", val);
+    },
+    value,
+  );
+});
+
 const api = {
   browse,
   screenshot,
+  input,
 };
 
-export const Do = async (G, {headless}) => {
+export const Do = async (G, {headless} = {headless: true}) => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disabled-setuid-sandbox"],
     headless,
@@ -32,10 +46,8 @@ export const Do = async (G, {headless}) => {
       return Promise.resolve();
     }
     await value(page);
-    const handle = await page.$("body");
     /* istanbul ignore next */
-    data = await page.evaluate((elem) => elem.innerHTML, handle);
-    await handle.dispose();
+    data = await page.$eval("body", (elem) => elem.innerHTML);
 
     return chain(nextG);
   };
--- test/do.js
+++ test/do.js
@@ -1,4 +1,5 @@
 import test from "ava";
+import {JSDOM} from "jsdom";
 import server from "./helpers/server";
 import {Do} from "../src";
 
@@ -10,11 +11,22 @@ test.after((t) => t.context.server.close());
 
 test("browse to a page", async (t) => {
   const {port} = t.context;
-  await Do(
-    function* simpleTest({browse}) {
-      const body = yield browse(`http://localhost:${port}/test.html`);
-      t.is("<h1>Hello World!</h1>", body);
-    },
-    {headless: true},
-  );
+  await Do(function* simpleTest({browse}) {
+    const body = yield browse(`http://localhost:${port}/test.html`);
+    const {window} = new JSDOM(body);
+    t.is("Hello World!", window.document.querySelector("h1").textContent);
+  });
+});
+
+test("fill text into an input field", async (t) => {
+  const {port} = t.context;
+  await Do(function* formInput({browse, input}) {
+    yield browse(`http://localhost:${port}/forms.html`);
+    const body = yield input("#input-field > input", "Hey You!");
+    const {window} = new JSDOM(body);
+    t.is(
+      "Hey You!",
+      window.document.querySelector("#input-field > input").value,
+    );
+  });
 });
